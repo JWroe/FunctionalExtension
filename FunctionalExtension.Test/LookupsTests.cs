39e3c04cebcdc51f03c28bd962b510be5ccabd2c
Jamie Wroe <jwroe@users.noreply.github.com>
2019-09-14 21:31:24 +0100

    lookup item in list

--- FunctionalExtension.Test/LookupsTests.cs
+++ FunctionalExtension.Test/LookupsTests.cs
@@ -11,23 +11,33 @@ namespace FunctionalExtension.Test
         [Theory]
         [InlineData("name", "value")]
         [InlineData("something else", "not found")]
-        public void NameValueCollectionLookup(string input, string expected)
-        {
-            var collection = new NameValueCollection { { "name", "value" } };
-            collection.Lookup(input)
-                      .Match(some => some, none => "not found")
-                      .ShouldBe(expected);
-        }
-        
+        public void NameValueCollectionLookup(string input, string expected) =>
+            new NameValueCollection { { "name", "value" } }.Lookup(input)
+                                                           .Match(some => some, none => "not found")
+                                                           .ShouldBe(expected);
+
         [Theory]
         [InlineData("key", "value")]
         [InlineData("not key", "not found")]
-        public void DictionaryLookup(string input, string expected)
-        {
-            var collection = new Dictionary<string, string> { { "key", "value" } };
-            collection.Lookup(input)
-                      .Match(some => some, none => "not found")
-                      .ShouldBe(expected);
-        }
+        public void DictionaryLookup(string input, string expected) =>
+            new Dictionary<string, string> { { "key", "value" } }.Lookup(input)
+                                                                 .Match(some => some, none => "not found")
+                                                                 .ShouldBe(expected);
+
+        [Theory]
+        [InlineData("one", "one")]
+        [InlineData("can't find me'", "not found")]
+        public void FirstOrNoneTest(string wordToFind, string expected) =>
+            new List<string> { "one", "two", "three" }.Lookup(word => word == wordToFind)
+                                                      .Match(some => some, none => "not found")
+                                                      .ShouldBe(expected);
+
+        [Theory]
+        [InlineData(1, 1)]
+        [InlineData(15, -1)]
+        public void FirstOrNoneTestStructs(int wordToFind, int expected) =>
+            new List<int> { 1 }.Lookup(word => word == wordToFind)
+                               .Match(some => some, none => -1)
+                               .ShouldBe(expected);
     }
 }
--- FunctionalExtension/Core/CollectionLookup.cs
+++ FunctionalExtension/Core/CollectionLookup.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Collections.Specialized;
+using System.Linq;
 
 namespace FunctionalExtension.Core
 {
@@ -7,5 +9,12 @@ namespace FunctionalExtension.Core
     {
         public static Option<TVal> Lookup<TKey, TVal>(this IDictionary<TKey, TVal> dict, TKey key) where TKey : notnull => dict.TryGetValue(key, out var value) ? Some(value).AsOption() : None();
         public static Option<string> Lookup(this NameValueCollection @this, string key) => @this[key];
+
+        public static Option<T> Lookup<T>(this IEnumerable<T> collection, Func<T, bool> predicate)
+        {
+            var items = collection.Where(predicate);
+            // ReSharper disable twice PossibleMultipleEnumeration - we are only looking at the first element, so it's better to do that twice than iterate over the whole collection once
+            return items.Any() ? Some(items.First()) : None().AsOption<T>();
+        }
     }
 }
